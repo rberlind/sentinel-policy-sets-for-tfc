6cfc0983b70472879923742fd393dd9bd417a4e6
rberlind <roger@hashicorp.com>
2019-09-21 10:52:43 -0400

    add local GH action

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM thrashr888/sentinel-simulator
+
+LABEL "com.github.actions.name"="sentinel test"
+LABEL "com.github.actions.description"="Test Sentinel policies"
+LABEL "com.github.actions.icon"="terminal"
+LABEL "com.github.actions.color"="blue"
+
+LABEL "repository"="https://github.com/rberlind/sentinel-policy-sets-for-tfc"
+LABEL "homepage"="http://github.com/rberlind/sentinel-policy-sets-for-tfc"
+LABEL "maintainer"="Roger Berlind"
+
+RUN apk --no-cache add jq curl
+
+COPY entrypoint.sh /entrypoint.sh
+COPY sentinel-policy-sets sentinel-policy-sets
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["."]
---
+++ entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+echo "pwd: $(pwd)"
+echo "ls: $(ls)"
+echo "env: $(env)"
+echo "STL_ACTION_WORKING_DIR: $STL_ACTION_WORKING_DIR"
+echo "Working Directory: $1"
+
+set -e
+cd "$1"
+echo "pwd after cd: $(pwd)"
+
+set +e
+OUTPUT=$(sh -c "sentinel test -verbose" 2>&1)
+SUCCESS=$?
+echo "$OUTPUT"
+set -e
+
+if [ $SUCCESS -eq 0 ]; then
+    exit 0
+fi
+
+if [ "$STL_ACTION_COMMENT" = "1" ] || [ "$STL_ACTION_COMMENT" = "false" ]; then
+    exit $SUCCESS
+fi
+
+FMT_TEST=$(echo "$OUTPUT" | sed -e "s,$(printf '\033')\\[[0-9;]*[a-zA-Z],,g")
+COMMENT="#### \`sentinel test\` Failed
+\`\`\`
+$FMT_TEST
+\`\`\`"
+PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+
+exit $SUCCESS
